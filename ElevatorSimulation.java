8048dc48094b43ca12ddc8361c3ff7292e436c80
jjprykiel <73499608+jjprykiel@users.noreply.github.com>
2022-04-27 13:41:51 -0400

    logic for main
    loop for main and making lists

--- ElevatorSimulation.java
+++ ElevatorSimulation.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 class ElevatorSimulation {
-    public static void main(String[] args){
+    public static void main(final String[] args) {
+        List fel = new List();
+        List delayed_list = new List();
         
+        while(! fel.get(0).equals("end_of_simulation")){
+            delayed_list.add(fel.get(0));
+            fel.add(Event) // generate new arrival (passenger generation)
+            fel.add(Event) // elevator event
+        }
     }
 }
